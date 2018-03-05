a432f63530e0b035d7919518e01c18de97455a79
cs2user <mkim7@caltech.edu>
2018-03-04 22:31:23 -0800

    made simple ai 1.0

--- player.cpp
+++ player.cpp
@@ -11,7 +11,30 @@
  */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
+
     testingMinimax = false;
+
+
+    for(int x = 0; x < 8; x++)
+    {
+        for(int y = 0; y < 8; y ++)
+        {
+            checked[x][y] = false;
+        }
+    }
+
+    myside = side;
+
+    cerr << myside  << endl;
+
+    if(myside == BLACK)
+    {
+        otherside = WHITE;
+    }
+    else
+    {
+        otherside = BLACK;
+    }
 
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
@@ -44,5 +67,31 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
+
+    //processing opponenet's move
+
+    //processing my move
+
+    brd.doMove(opponentsMove, otherside);
+
+
+    if (brd.isDone() || !brd.hasMoves(myside))
+    {
+        return nullptr;
+    }
+
+    while(true)
+    {
+        int x  = rand() % 8;
+        int y = rand() % 8;
+        Move *m = new Move(x, y);
+        if (brd.checkMove(m, myside))
+        {
+            brd.doMove(m, myside);
+            return m;
+        }
+    }
+
+
     return nullptr;
 }
